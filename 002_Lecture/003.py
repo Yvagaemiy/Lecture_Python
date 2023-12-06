a12839b307eca322c1c097a7d5cf2edc50c536a5
Yvagaemiy <krylatiy2@mail.ru>
2023-12-06 15:14:56 +0300

    Lecture

--- 002_Lecture/003.py
+++ 002_Lecture/003.py
@@ -4,15 +4,16 @@
 
 #!!!!! Список определяется квадратными []- скобками!!!!!!!
 
-list_4 = [1,2,3,4,5,6,7,8,9,10]
-print(list_4[0])
-print(list_4[1])
-print(list_4[len(list_4)-1])
-print(list_4[-5])
-print(list_4[:])
-print(list_4[:2])
-print(list_4[len(list_4)-2:])
-print(list_4[2:9])
-print(list_4[6:-18])
-print(list_4[0:len(list_4):6])
-print(list_4[::6])
+
+list_4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_4[0]) # 1
+print(list_4[1]) # 2
+print(list_4[len(list_4)-1]) # 10
+print(list_4[-5]) # 6
+print(list_4[:]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_4[:2]) # [1, 2]
+print(list_4[len(list_4)-2:]) #[9, 10]
+print(list_4[2:9]) # [3, 4, 5, 6, 7, 8, 9]
+print(list_4[6:-18]) # []
+print(list_4[0:len(list_4):6]) # [1, 7]
+print(list_4[::6]) # [1, 7]
